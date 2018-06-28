36bee417cf910a3d9250a61872d8b113ba6782cf
Reyes Ruiz <ruiz.reyes@yahoo.com>
2018-06-28 15:34:30 -0700

    Chapter 1, enhanced fetch

--- src/fetch/fetch.go
+++ src/fetch/fetch.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 )
@@ -14,12 +14,14 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-		b, err := ioutil.ReadAll(resp.Body)
+		b, err := io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+		status := resp.StatusCode
+		fmt.Printf("%s\n", b)
+		fmt.Printf("Status code: %d\n", status)
 	}
 }
